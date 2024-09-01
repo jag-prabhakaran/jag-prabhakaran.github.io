df86b3dabd59e66a9e75a57656a7fa375d4eb7fc
jag-prabhakaran <jag.prabhakaran@gmail.com>
2024-08-31 20:54:22 -0700

    massive

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const themeToggleButton = document.getElementById('theme-toggle');
-    const storedTheme = localStorage.getItem('theme') || (window.matchMedia("(prefers-color-scheme: dark)").matches ? 'dark' : 'light');
-    
-    document.documentElement.setAttribute('data-theme', storedTheme);
-    themeToggleButton.checked = storedTheme === 'dark';
+document.addEventListener("DOMContentLoaded", function () {
+  const themeToggleButton = document.getElementById("theme-toggle");
+  const storedTheme =
+    localStorage.getItem("theme") ||
+    (window.matchMedia("(prefers-color-scheme: dark)").matches
+      ? "dark"
+      : "light");
 
-    themeToggleButton.addEventListener('change', function () {
-        const newTheme = themeToggleButton.checked ? 'dark' : 'light';
-        document.documentElement.setAttribute('data-theme', newTheme);
-        localStorage.setItem('theme', newTheme);
-    });
+  document.documentElement.setAttribute("data-theme", storedTheme);
+  themeToggleButton.checked = storedTheme === "dark";
+
+  themeToggleButton.addEventListener("change", function () {
+    const newTheme = themeToggleButton.checked ? "dark" : "light";
+    document.documentElement.setAttribute("data-theme", newTheme);
+    localStorage.setItem("theme", newTheme);
+  });
 });
